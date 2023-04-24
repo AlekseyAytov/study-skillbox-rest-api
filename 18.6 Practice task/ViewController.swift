ab116ea25396a286e1fdc24b3713e8db0c9ab511
AlekseyAytov <aytov88@gmail.com>
2023-04-24 15:52:36 +0300

    Initial Commit

---
+++ 18.6 Practice task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  18.6 Practice task
+//
+//  Created by Alex Aytov on 4/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
