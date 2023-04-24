5532f256973c2b2d2171e362c0b979bd3daddd72
AlekseyAytov <aytov88@gmail.com>
2023-04-24 16:07:10 +0300

    Initial commit

---
+++ 18.6 Practice task/Cells/MainCustomTVC.swift
@@ -0,0 +1,23 @@
+//
+//  MainCustomTVC.swift
+//  18.6 Practice task
+//
+//  Created by Alex Aytov on 4/24/23.
+//
+
+import UIKit
+
+class MainCustomTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
